719a6a78c8103a0d77925bc58742f495b67415fd
JangSH0 <jsh6456@minirecord.com>
2025-04-29 18:20:52 +0900

    Initial commit

---
+++ src/main/java/com/honomoly/garbages/GarbagesApplication.java
@@ -0,0 +1,13 @@
+package com.honomoly.garbages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarbagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarbagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/honomoly/garbages/GarbagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.honomoly.garbages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarbagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
